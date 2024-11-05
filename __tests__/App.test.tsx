a86ae2fe11ccfd85cc62394724727ec24378159d
anjanibarlapati <anjani.barlapati@everst.engineering>
2024-11-06 01:04:02 +0530

    added all files

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -5,12 +5,12 @@
 import 'react-native';
 import React from 'react';
 import {describe, test} from '@jest/globals';
-import {render} from 'react-test-renderer';
+import renderer from 'react-test-renderer';
 import App from '../App';
 
 
 describe("App Component", ()=>{
   test('Should render welcome message', () => {
-    const { getByPlaceholderText, getByText } = render(<App />);  });
-
+       renderer.create(<App/>);
 })
+});
